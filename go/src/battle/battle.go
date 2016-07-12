e612ed5d7f976320d7b0559f752e138e3592a1e2
Cole Vahey <rcvahey01@gmail.com>
2016-07-11 20:30:40 -0400

    save

--- go/src/battle/battle.go
+++ go/src/battle/battle.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	c "github.com/skilstak/go-colors"
 	"github.com/skilstak/go-input"
+	"strings"
 )
 
 func main() {
 	fmt.Println(c.Clear + c.B3 + "GOBATTLE")
-	var player = input.Ask("What is your player name? >> " + c.Blue)
-	fmt.Println(c.B3 + "Welcome " + player)
+	var player = input.Ask("What is your player name? >> " + c.Green)
+	fmt.Println(c.B3 + "Welcome " + c.Green + strings.Title(player))
 }
